3d6c204217ce875152d3f2df41768ad1b5131b2c
Iskandar <iskandarismoili@gmail.com>
2022-01-23 15:00:54 +0500

    changed function properties and added cooking process

--- menu.php
+++ menu.php
@@ -1,5 +1,13 @@
 <?php
 
-function cookMeatballs(float $riceInGrams, float $eggInpPieces, float $cheeseInGrams, int $dillInBranches, float $saltInGrams, float $vegetableOilInMilliliters, int $breadCrumbsInGlasses){
-    
+function cookRiceballs(float $riceInGrams, int $eggInPieces, float $cheeseInGrams, int $dillInBranches, float $saltInGrams, float $vegetableOilInMilliliters, string $feedbacks){
+    echo "Boil ", $riceInGrams, " grams of rice normally. Cool and add ", $eggInPieces ," eggs. ";
+    echo "We rub ", $cheeseInGrams ," grams of cheese on a fine grater. ";
+    echo "Adding ", $saltInGrams, " grams of salt to rice. ";
+    echo "Adding grated cheese. ";
+    echo "Chop ", $dillInBranches ," branches of greens and also add to the rice. ";
+    echo "We form riceballs, bread them in flour. ";
+    echo "Fry them on both sides in ", $vegetableOilInMilliliters ," milliliters of vegetable oil on both sides. ";
+    echo "Riceballs are ready. ";
+    echo $feedbacks, ".";
 }
