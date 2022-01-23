07aa069135c244107f7a9b43c27c4b30f490f91f
Iskandar <iskandarismoili@gmail.com>
2022-01-23 16:05:20 +0500

    changed names of values

--- menu.php
+++ menu.php
@@ -1,14 +1,14 @@
 <?php
 
-function cookRiceballs(float $riceInGrams, int $eggInPieces, float $cheeseInGrams, int $dillInBranches, float $saltInGrams, float $vegetableOilInMilliliters, string $feedbacks){
-    echo "Boil ", $riceInGrams, " grams of rice normally. Cool and add ", $eggInPieces ," eggs. ";
-    echo "We rub ", $cheeseInGrams ," grams of cheese on a fine grater. ";
-    echo "Adding ", $saltInGrams, " grams of salt to rice. ";
+function cook_riceballs(float $rice, int $egg, float $cheese, int $dill, float $salt, float $vegetable_oil, string $feedbacks){
+    echo "Boil ", $rice, " grams of rice normally. Cool and add ", $egg ," eggs. ";
+    echo "We rub ", $cheese ," grams of cheese on a fine grater. ";
+    echo "Adding ", $salt, " grams of salt to rice. ";
     echo "Adding grated cheese. ";
-    echo "Chop ", $dillInBranches ," branches of greens and also add to the rice. ";
+    echo "Chop ", $dill ," branches of greens and also add to the rice. ";
     echo "We form riceballs, bread them in flour. ";
-    echo "Fry them on both sides in ", $vegetableOilInMilliliters ," milliliters of vegetable oil on both sides. ";
+    echo "Fry them on both sides in ", $vegetable_oil ," milliliters of vegetable oil on both sides. ";
     echo "Riceballs are ready. ";
     echo $feedbacks, ".";
 }
-cookRiceballs(250.0, 1, 100.0, 3, 30.0, 100.0, "That's grate");
+cook_riceballs(250.0, 1, 100.0, 3, 30.0, 100.0, "That's grate");
