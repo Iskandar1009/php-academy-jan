97981197d50d3b6223160accdf15c9e8bd528409
Iskandar <iskandarismoili@gmail.com>
2022-01-23 14:23:38 +0500

    create function cookMeatballs

---
+++ menu.php
@@ -0,0 +1,5 @@
+<?php
+
+function cookMeatballs(float $riceInGrams, float $eggInpPieces, float $cheeseInGrams, int $dillInBranches, float $saltInGrams, float $vegetableOilInMilliliters, int $breadCrumbsInGlasses){
+    
+}
